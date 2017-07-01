9ccda7384a2b286086b55689daf32eebb9e9e549
Kamal <kamaldolikay@gmail.com>
2017-07-01 16:12:38 +0530

    login service

--- api/services/Registeration.js
+++ api/services/Registeration.js
@@ -4,12 +4,44 @@ var bcrypt    = require('bcrypt-nodejs');
 module.exports = {
 
     register: function(data){
-        console.log("data", data)
+        console.log("data", data);
         data.password = bcrypt.hashSync(data.password, bcrypt.genSaltSync(10));
         return API.Model(Users).create(data).then(function(user){
             return {
                 message: "success"
+            };
+        });
+    },
+
+    login: function(data){
+        console.log("data",data);
+        let username = data.username;
+        return API.Model(Users).findOne({username:username}).then(function(user){
+            console.log("user",user);
+            if( user === undefined ){
+                return {
+                    success: false,
+                    error: {
+                        code: 404,
+                        message: "user not found"
+                    }
+                };
             }
-        })
+
+            if( !bcrypt.compareSync(data.password, user.password) ){
+                return {
+                    success: false,
+                    error: {
+                        code: 404,
+                        message: "incorrect password"
+                    }
+                };
+            } else {
+                return Tokens.generateToken({
+                        user_id: user.id,
+                        client_id: Tokens.generateTokenString()
+                    });
+            }
+        });
     }
 }
