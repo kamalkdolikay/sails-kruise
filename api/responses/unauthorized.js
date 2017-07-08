0b179c2e2647025d76a71daa60f14c17c117f7d3
Kamal <kamaldolikay@gmail.com>
2017-07-08 18:42:20 +0530

    custom response for unauthorized access

---
+++ api/responses/unauthorized.js
@@ -0,0 +1,17 @@
+/**
+ * 401 (Unauthorized) Response
+ * Similar to 403 Forbidden.
+ * Specifically for authentication failed or not yet provided.
+ */
+module.exports = function (data, code, message, root) {
+  var response = _.assign({
+    code: code || 'E_UNAUTHORIZED',
+    message: message || 'Missing or invalid authentication token',
+    data: data || {}
+  }, root);
+
+  this.req._sails.log.silly('Sent (401 UNAUTHORIZED)\n', response);
+
+  this.res.status(401);
+  this.res.jsonx(response);
+};
