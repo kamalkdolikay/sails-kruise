ec2a9a33b2b727c0f15a964a4fab67d2818b7e5c
Kamal <kamaldolikay@gmail.com>
2017-07-08 23:50:12 +0530

    token generation for oauth2

--- api/models/Tokens.js
+++ api/models/Tokens.js
@@ -5,90 +5,174 @@
  * @docs        :: http://sailsjs.org/documentation/concepts/models-and-orm/models
  */
 
-var promisify = require('bluebird').promisify;
-var randToken = require('rand-token');
+var Promise = require('bluebird'),
+    promisify = Promise.promisify,
+    randToken = require('rand-token');
+
 
 module.exports = {
 
-  attributes: {
 
-    access_token: {
-      type: 'string',
-      required: true,
-      unique: true
+    attributes: {
+
+        access_token: {
+            type: 'string',
+            required: true,
+            unique: true
+        },
+
+        refresh_token: {
+            type: 'string',
+            required: true,
+            unique: true
+        },
+
+        code: {
+            type: 'string',
+            unique: true
+        },
+
+        user_id: {
+            type: 'string'
+        },
+
+        expiration_date: {
+            type: 'date'
+        },
+
+        client_id: {
+            type: 'string',
+            required: true
+        },
+
+        security_level: {
+            type: 'string'
+        },
+
+        scope: {
+            type: 'string'
+        },
+
+        calc_expires_in: function () {
+            return Math.floor(new Date(this.expiration_date).getTime() / 1000 - new Date().getTime() / 1000);
+        },
+
+        toJSON: function () {
+            var hiddenProperties = ['id','access_token','refresh_token','code','user_id','client_id'],
+                obj = this.toObject();
+
+            obj.expires_in = this.expires_in();
+
+            hiddenProperties.forEach(function(property){
+                delete obj[property];
+            });
+
+            return obj;
+        }
+
     },
 
-    refresh_token: {
-      type: 'string',
-      required: true,
-      unique: true
+
+    authenticate: function (criteria) {
+        var tokenInfo,
+            $Tokens = API.Model(Tokens),
+            $Users = API.Model(Users),
+            $Clients = API.Model(Clients),
+            $result;
+
+        if (criteria.access_token) {
+            $result = $Tokens.findOne({access_token: criteria.access_token});
+        }
+        else if (criteria.code) {
+            $result = $Tokens.findOne({code: criteria.code});
+        }
+        else {
+            //Bad Token Criteria
+            return Promise.reject("Unauthorized");
+        }
+
+        return $result.then(function (token) {
+
+            if (!token) return null;
+
+            // Handle expired token
+            if (token.expiration_date && new Date() > token.expiration_date) {
+                return $Tokens.destroy({access_token: token}).then(function () {
+                    return null
+                });
+            }
+
+            tokenInfo = token;
+            if (token.user_id != null) {
+                return $Users.findOne({id: token.user_id});
+            }
+            else {
+                //The request came from a client only since userID is null
+                //therefore the client is passed back instead of a user
+                return $Clients.findOne({client_id: token.client_id});
+            }
+
+        }).then(function (identity) {
+
+            // to keep this example simple, restricted scopes are not implemented,
+            // and this is just for illustrative purposes
+
+
+            if (!identity) return null;
+            else if (criteria.type == 'verification') {
+                if (identity.email != criteria.email) return null;
+            }
+            // Otherwise if criteria.type != 'verfication'
+            else if (!identity.date_verified) return null;
+
+            return {
+                identity: identity,
+                authorization: {
+                    scope: tokenInfo.scope,
+                    token: tokenInfo
+                }
+            };
+        });
     },
 
-    code: {
-      type: 'string',
-      unique: true
+    generateTokenString: function () {
+        return randToken.generate(sails.config.security.oauth.token.length);
     },
+    generateToken: function (criteria) {
 
-    user_id: {
-      type: 'string'
-    },
+        //if (err) return next(err);
 
-    expiration_date: {
-      type: 'date'
-    },
+        var token = {},
+            accessToken,
+            $Tokens = API.Model(Tokens);
 
-    client_id: {
-      type: 'string',
-      required: true
-    },
+        if (!criteria.client_id) return Promise.resolve(null);
 
-    security_level: {
-      tyepe: 'string'
-    },
+        token.client_id = criteria.client_id;
+        token.user_id = criteria.user_id || undefined;
 
-    scope: {
-      type: 'string'
-    }
 
-  },
+        token.access_token = accessToken = Tokens.generateTokenString();
 
-  expires_in: function (){
-      return Math.floor(new Date(this.expiration_date).getTime() / 1000 - new Date().getTime() / 1000)
-    },
+        token.refresh_token = Tokens.generateTokenString();
+        token.code = Tokens.generateTokenString();
 
-    toJSON: function (){
-      var hiddenValues = [ 'id', 'access_token', 'refresh_token', 'code', 'user_id', 'client_id' ];
-          obj = this.toObject();
+        if (!criteria.expiration_date) {
+            token.expiration_date = new Date();
+            token.expiration_date.setTime(token.expiration_date.getTime() + sails.config.security.oauth.token.expiration * 1000 + 999);
+        }
 
-      obj.expires_in = this.expires_in();
 
-      hiddenValues.forEach(function(property){
-        console.log("property", property)
-      });
+        return $Tokens.findOrCreate(criteria, token).then(function (retrievedToken) {
 
-      return obj;
-    },
 
-    generateTokenString: function (){
-      return randToken.generate(sails.config.security.oauth.token.length);
-    },
+            if (retrievedToken.access_token != accessToken) {
+                return $Tokens.update(criteria, token).then(function (updatedTokens) {
+                    return updatedTokens[0];
+                });
+            }
+            return retrievedToken;
+        });
 
-    generateToken: function ( data ){
-      console.log("data", data)
-      var token = {},
-          accessToken,
-          $Token = API.Model(Tokens);
-
-      token.client_id = data.client_id;
-      token.user_id = data.user_id;
-      token.access_token = this.generateTokenString();
-      token.refresh_token = this.generateTokenString();
-      token.code = this.generateTokenString();
-
-      return $Token.findOrCreate(data, token).then(function(result){
-        console.log("result", result);
-        return token;
-      })
     }
 };
-
