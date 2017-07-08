f977dd76ac5209b02bd2eb87d11926dddecb535d
Kamal <kamaldolikay@gmail.com>
2017-07-08 18:33:20 +0530

    updated models with new validations

--- api/models/Users.js
+++ api/models/Users.js
@@ -9,12 +9,13 @@ var promisify = require('bluebird').promisify;
 var bcrypt    = require('bcrypt-nodejs');
 
 module.exports = {
-
+  schema: true,
   attributes: {
-
       username: {
         type: 'string',
-        required: true
+        required: true,
+        unique: true,
+        alphanumericdashed: true
       },
 
       password: {
@@ -22,7 +23,52 @@ module.exports = {
         required: true,
         columnName: 'encryptedPassword',
         minLength: 8
+      },
+
+      email: {
+        type: 'string',
+        email: true,
+        required: true,
+        unique: true
+      },
+
+      firstName: {
+        type: 'string',
+        defaultsTo: ''
+      },
+
+      lastName: {
+        type: 'string',
+        defaultsTo: ''
+      },
+
+      image: {
+        type: 'string',
+        defaultsTo: '',
+        url: true
+      },
+
+      socialProfiles: {
+        type: 'object',
+        defaultsTo: {}
+      },
+
+      toJSON: function(){
+        var obj = this.toObject();
+        delete obj.password;
+        delete obj.socialProfiles;
+        return obj;
       }
+  },
+
+  beforeUpdate: function(values, next){
+    CipherService.hashPassword(values);
+    next();
+  },
+
+  beforeCreate: function(values, next){
+    CipherService.hashPassword(values);
+    next();
   }
 };
 
