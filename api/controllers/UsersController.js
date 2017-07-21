7c3ed5732fa375555ad9633f9809252b66b9aa14
KD <kamaldolikay@gmail.com>
2017-07-21 19:11:49 +0530

    removed login and register

--- api/controllers/UsersController.js
+++ api/controllers/UsersController.js
@@ -7,24 +7,16 @@
 
 module.exports = {
 
-    register: function(req, res){
-        API(Registration.registerUser, req, res);
-    },
-
     'verify/:email': function(req,res){
-        API(Registration.verifyUser,req,res);
+        API(UsersService.verifyUser,req,res);
     },
 
     current: function(req,res){
-        API(Registration.currentUser,req,res);
-    },
-
-    login: function(req, res){
-        API(Registration.login, req, res);
+        API(UsersService.currentUser,req,res);
     },
 
     signin: function(req, res){
-        API(Registration.signinUser, req, res);
+        API(UsersService.signinUser, req, res);
     }
 };
 
